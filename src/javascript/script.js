d619302fdc96675d05ac2abeb620cb9bf8f84372
Falih <falihdzakwan028@gmail.com>
2024-10-19 23:12:32 +0700

    enhance using microformat

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -3,18 +3,19 @@ async function fetchUsers() {
         const response = await fetch('https://randomuser.me/api/?results=50&nat=us');
         const data = await response.json();
         const users = data.results;
-
         const tbody = document.querySelector('tbody');
 
         users.forEach((user, index) => {
             const row = document.createElement('tr');
+            row.setAttribute('itemscope', '');
+            row.setAttribute('itemtype', 'https://schema.org/Person');
             row.innerHTML = `
-        <td>${index + 1}</td>
-        <td>${user.name.first} ${user.name.last}</td>
-        <td>${user.email}</td>
-        <td>${new Date(user.dob.date).toLocaleDateString()}</td>
-        <td>${user.gender.charAt(0).toUpperCase() + user.gender.slice(1)}</td>
-      `;
+                <td scope="row" itemprop="identifier">${index + 1}</td>
+                <td itemprop="name">${user.name.first} ${user.name.last}</td>
+                <td itemprop="email">${user.email}</td>
+                <td itemprop="birthDate">${new Date(user.dob.date).toLocaleDateString()}</td>
+                <td itemprop="gender">${user.gender.charAt(0).toUpperCase() + user.gender.slice(1)}</td>
+            `;
             tbody.appendChild(row);
         });
     } catch (error) {
